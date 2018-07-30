a4277ba26db0bd8bed99929ce838e4305e9a2536
HaoZeke <rohit.goswami@aol.com>
2018-07-30 19:30:37 +0530

    emacs: Add in-file instructions

--- lammps-mode.el
+++ lammps-mode.el
@@ -33,6 +33,16 @@
 ;; translation of keyword classes from tools/vim
 ;; see http://xahlee.org/emacs/elisp_syntax_coloring.html
 
+;; Put this in your .emacs file to enable autoloading of lammps-mode
+;; and auto-recognition of "in.*" and "*.lmp" files:
+;;
+;; (autoload 'lammps-mode "lammps-mode.el" "LAMMPS mode." t)
+;; (setq auto-mode-alist (append auto-mode-alist
+;;                               '(("in\\." . lammps-mode))
+;;                               '(("\\.lmp\\'" . lammps-mode))
+;;                               ))
+;;
+
 ;;; Code:
  ;; define several keyword classes
 (defvar lammps-output
